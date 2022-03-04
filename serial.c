1407c364270cbd1aded67407c8299d2851218e1e
muesal <salome.mueller@alansa.ch>
2022-03-04 12:57:19 +0100

    Serial recursive algorithm

--- serial.c
+++ serial.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /**
@@ -24,7 +25,7 @@ int queens(int n);
  * @param i: next row to set queen in
  * @return int, the number of solutions reachable from given a
  **/
-int queens_rec(int n, int a[], int i);
+void queens_rec(int n, int a[], int row, int *solutions);
 
 
 /**
@@ -37,12 +38,13 @@ int queens_rec(int n, int a[], int i);
  * @param i: row of last queen that was set
  * @return boolean whether the (part-)solution is valid
  **/
-bool is_valid(int a[], int i);
+bool is_valid(int a[], int row);
 
 
 int main(int argc, char **argv) {
     int n = argc > 1 ? atoi(argv[1]) : 2;
     
+    // TODO: add timer
     int solutions = queens(n);
     printf("Number of solutions with %d queens: %d\n",
         n, solutions);
@@ -52,48 +54,39 @@ int main(int argc, char **argv) {
 int queens(int n) {
 
     int solutions = 0;
+    int *a = (int *) malloc(n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        int* a = &i; // set first queen to column i
-        solutions += queens_rec(n, a, 1);
-    }
-
-    return solutions;
-
-}
-
-int queens_rec(int n, int a[], int i) {
-    // base case: return 1 (only valid solutions are passed on)
-    if (i >= n){
-        for (int q = 0; q < n; q++) {
-            printf(" %d", a[q]);
-        }
-        printf("\n");
-        return 1;
-    }
-
-    int solutions = 0;
-
-    for (int j = 0; j < n; j++) {
-
-        int *a_new = (int *) malloc(i * sizeof(int));
-        a_new = a; // deep copy of array, I think
-        a_new[i] = j;
-
-        if (is_valid(a_new, i)) {
-            solutions += queens_rec(n, a_new, i + 1);
-        }
+    for (int col = 0; col < n; col++) {
+        // set queen in first row to column col
+        a[0] = col;
+        queens_rec(n, a, 1, &solutions);
     }
 
     return solutions;
 }
 
-bool is_valid(int a[], int i) {
-    for (int j = 0; j < i; j++) {
-        if (a[j] == a[i] // same column
-            || a[i] - a[j] == i - j // same diagonally
-            || j - a[j] == i - a[i] // same diagonally
-            || j - a[j] == a[i]- i) // same diagonally
+// TODO: n-row instead of n and row; only one value. base case if 0
+void queens_rec(int n, int a[], int row, int *solutions) {
+    
+    if (row >= n){
+        // base case: return 1
+        // noo need to validate since only valid solutions are passed on
+        (*solutions)++;
+    } else {
+        for (int col = 0; col < n; col++) {
+            a[row] = col;
+
+            if (is_valid(a, row))
+                queens_rec(n, a, row + 1, solutions);
+        }
+    }
+}
+
+bool is_valid(int a[], int row) {
+    for (int r = 0; r < row; r++) {      // iterate over all previous rows
+        if (a[r] == a[row]               // same column
+            || r - a[r] == row - a[row]  // same diagonally
+            || r + a[r] == row + a[row]) // same diagonally
             return false;
     }
 
